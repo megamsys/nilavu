79d1623938b13a02873ec89dd449d78803377e4d
Alrin <alrin@alrin-work.(none)>
2012-08-21 13:07:07 +0530

    Cloud_Identity Creation, Updation and deletion

--- app/controllers/cloud_identities_controller.rb
+++ app/controllers/cloud_identities_controller.rb
@@ -1,15 +1,36 @@
 class CloudIdentitiesController < ApplicationController
+
 def index
 end
 
 def create
+	
 	params = {"cloud_identity" => {"url" => "www.google.com"}}
-	@cloud_identity = CloudIdentity.new(params[:cloud_identity])
+        @cloud_identity = current_user.organization.build_cloud_identity(params[:cloud_identity]) || CloudIdentity.new(params[:cloud_identity])
+	#params = {"cloud_identity" => {"url" => "www.google.com"}}
+	#@cloud_identity = CloudIdentity.new(params[:cloud_identity])
 	if @cloud_identity.save
 		redirect_to customizations_show_url
 	end
 end
 
 def show
+	 @user = User.find(params[:id])
+
+end
+
+def update
+	@cloud_identity=CloudIdentity.find(params[:id])
+	if @cloud_identity.update_attributes(params[:cloud_identity])
+     		 flash[:success] = "Cloud_identity updated"
+	end
+end
+
+
+def destroy
+	
+    CloudIdentity.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to customizations_show_url
   end
 end
