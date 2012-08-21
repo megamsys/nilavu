871b2160ef85e2e29627821a17ea961eaa972385
Kishorekumar Neelamegam <indykish@gmail.com>
2012-08-21 13:22:48 +0530

    Updated finish method of the script.

--- script/d.sh
+++ script/d.sh
@@ -233,7 +233,7 @@ finish(){
 		help
         	exitScript 1
 	else
-                echo "Howdy committer.Enjoy merging <$IDP_GIT_COMMIT_MASTER> to origin..."
+        echo "Howdy committer.Enjoy merging <$IDP_GIT_COMMIT_MASTER> to origin..."
 		echo "========================================================="
 		echo -n "Do you want to add/commit files [y/n]? "
 		read -n 1 addcommit
@@ -246,12 +246,14 @@ finish(){
 			git commit .
 		fi
 		echo "========================================================="
-		echo -n "Do you want to commit $IDP_COMMIT_MASTER to your origin [y/n]? "
+		echo -n "Do you want to commit IDP_GIT_COMMIT_MASTER to your origin [y/n]? "
 		read -n 1 pmstr
 		echo
 		if [[ $pmstr =~ ^[Yy]$ ]]
 		then
 			echo "========================================================="
+			git fetch $IDP_COMMIT_MASTER
+			git merge $IDP_COMMIT_MASTER/master
 			git push -u origin $IDP_COMMIT_MASTER/master
 			echo "========================================================="
 		fi
