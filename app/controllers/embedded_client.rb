cdf6b21999613fc1ca2feec61cd6ef2e56bbd703
Thomas Alrin <alrin@thomas-work.(none)>
2012-09-07 14:09:23 +0530

    Embedded client- mcollective to stomp

--- app/controllers/embedded_client.rb
+++ app/controllers/embedded_client.rb
@@ -1,20 +1,12 @@
-require 'mcollective'
-
 class EmbeddedClient
-	include MCollective::RPC
-  def client
-   options =  MCollective::Util.default_options
-    options[:config] = 'config/mcollective.cfg'
-	#logger.debug "#{agent}"
-    MCollective::RPC::Client.new("cloudidentity", :options => options)
-    #client = rpcclient("cloudidentity", {:options => options})
-    client.discovery_timeout = 30
-    client.timeout = 60
-   client.listrealms(:msg => "Howdy megam") .each do |resp|
-   printf("%-40s: %s\n", resp[:sender], resp[:data][:msg])
-end
-    client.close 
- end
+ def client
+    client = Stomp::Client.new("hot#1rod", "mcollective", "ironfist.megam.co.in", 6163)
+    client.send("/topic/", "hello world!")
+    client.subscribe("/topic/") do |msg|
+ 	  p msg
+    end
+    client
+  end
 
 
   
