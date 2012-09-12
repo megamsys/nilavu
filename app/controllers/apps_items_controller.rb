27bc284b9c62ed1ade52de1daa8109cc46fa72bd
Thomas Alrin <alrin@thomas-work.(none)>
2012-09-12 12:06:03 +0530

    12-9-12

--- app/controllers/apps_items_controller.rb
+++ app/controllers/apps_items_controller.rb
@@ -11,18 +11,26 @@ end
 	@user = current_user
 	product = Product.find(params[:product_id])
 	@apps_item = @user.organization.cloud_app.apps_items.build(:product => product) || AppsItem.new(params[:apps_item])
-		
-	if @apps_item.save
-		redirect_to apps_item_path(current_user)
-		
-	end
+	 @apps_item.save
+flash[:sucess] = "Created Apps_items"
+    respond_to do |format|
+      format.html { redirect_to @apps_item }
+      format.js
+    end	
+     
+  
+	
   end
 def display
 end
 
   def destroy
      current_user.organization.cloud_app.apps_items.find(params[:id]).destroy
-   
-    redirect_to apps_item_path(current_user)
+    
+   # redirect_to apps_item_path(current_user)
+    respond_to do |format|
+      format.html { redirect_to @apps_item }
+      format.js
+    end
   end
 end
