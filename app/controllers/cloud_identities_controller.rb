dfcf2f083fba431b2b8a7d318acf4014f1015f45
Thomas Alrin <alrin@thomas-work.(none)>
2012-09-20 12:42:21 +0530

    Plaese spelling

--- app/controllers/cloud_identities_controller.rb
+++ app/controllers/cloud_identities_controller.rb
@@ -32,7 +32,7 @@ end
   def show
     @user = User.find(params[:id])
     if !@user.organization
-      flash[:error] = "Pleae Create Organization Details first"
+      flash[:error] = "Please Create Organization Details first"
       redirect_to edit_user_path(current_user)
     end
   end
