7a5c2411ea94d3621d8bbae2e5f6af3877e81a61
Thomas Alrin <alrin@thomas-work.(none)>
2012-10-04 19:58:13 +0530

    Connector creation

--- app/controllers/connectors_controller.rb
+++ app/controllers/connectors_controller.rb
@@ -1,18 +1,46 @@
 class ConnectorsController < ApplicationController
 
  def new
-   
-   current_user.organization.cloud_app.connectors.build(params[:connectors])
+       
+        
+    if !current_user.organization
+      flash[:error] = "Please Create Organization Details first"
+      redirect_to edit_user_path(current_user)
+    #elsif !current_user.organization.cloud_app	
+	#flash[:error] = "Please Add any cloud Applications"
+	#redirect_to new_apps_item_path 
+    else
+    #elsif !current_user.organization.cloud_app.connectors
+     	current_user.organization.cloud_app.connectors.build(params[:connectors])
 	@products = Product.all
 	@apps_item = current_user.organization.cloud_app.apps_items
-	@connector = current_user.organization.cloud_app.connectors
-	
-end
+    end
+ end
 
  def create
+	@user = current_user
+	@connector = @user.organization.cloud_app.connectors.build(params[:connector]) || Connector.new(params[:connector])
+	#@connector = current_user.organization.cloud_app.connectors.build(params[:connectors]) || Connector.new(params[:connectors])
   
-   @connector = current_user.organization.cloud_app.connectors.build(params[:connectors]) || Connector.new(params[:connector])
-   @connector.save
+   	if @connector.save
+	flash[:success] = "Connector Created."
+	#redirect_to customizations_show_url
+	@products = Product.all
+	@apps_item = current_user.organization.cloud_app.apps_items
+	render 'new'
+        end
  end
+
+
+
+ def update
+    @connector=Connector.find(params[:id])
+    if @connector.update_attributes(params[:connector])
+      flash[:success] = "Connector #{current_user.organization.account_name} updated"
+      @products = Product.all
+	@apps_item = current_user.organization.cloud_app.apps_items
+	render 'new'
+    end
+  end
 
 end
