e2f3d82fa89350c6eef2280a4beda46b8589193b
pandiyaraja <raja.pandiya@megam.co.in>
2012-10-04 13:40:55 +0530

    modified connectors controller

--- app/controllers/connectors_controller.rb
+++ app/controllers/connectors_controller.rb
@@ -2,17 +2,27 @@ class ConnectorsController < ApplicationController
 
  def new
    
-   current_user.organization.cloud_app.connectors.build(params[:connectors])
+   #current_user.organization.cloud_app.connectors.build(params[:connectors])
 	@products = Product.all
 	@apps_item = current_user.organization.cloud_app.apps_items
-	@connector = current_user.organization.cloud_app.connectors
+	#current_user.organization.cloud_app.connectors.build
 	
 end
 
  def create
   
    @connector = current_user.organization.cloud_app.connectors.build(params[:connectors]) || Connector.new(params[:connector])
-   @connector.save
+   if @connector.save
+     flash[:error] = "CONNECTOR CREATED"
+     redirect_to customizations_show_url
+   end
  end
+ def update
+    @connector=Connector.find(params[:id])
+    if @connector.update_attributes(params[:connector])
+      flash[:success] = "Connector #{current_user.organization.account_name} updated"
+      redirect_to customizations_show_url
+    end
+  end
 
 end
