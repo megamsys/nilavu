5439c4e682b8e8d909791c5b6c4abf021278e831
Thomas Alrin <alrin@thomas-work.(none)>
2012-10-25 19:03:26 +0530

    Connector_Execution with json

---
+++ app/models/connector_execution.rb
@@ -0,0 +1,14 @@
+class ConnectorExecution
+  include Dynamoid::Document
+  
+  
+  field :access_email
+  field :access_org_id
+  field :access_account_name
+  field :api_token
+  field :result
+ 
+
+
+  belongs_to :connector_project
+end
