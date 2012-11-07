72dc0f97fdd2c0baef208e6d2b8a80789647486c
Thomas Alrin <alrin@thomas-work.(none)>
2012-11-07 15:08:36 +0530

    Email verification done. DynamoDB local storage in port 80

--- Gemfile
+++ Gemfile
@@ -14,22 +14,25 @@ gem 'will_paginate','3.0.3'
 gem 'bootstrap-will_paginate','0.0.8'
 gem 'rest-client','1.6.7'
 gem 'grape','0.2.2'
-gem 'paperclip','3.3.0'
-gem 'aws-sdk', '~> 1.6.9'
-gem 'high_voltage','1.2.0'
+gem 'paperclip','3.3.1'
+gem 'aws-sdk', '~> 1.7.0'
+gem 'high_voltage','1.2.1'
 gem "therubyracer", '0.10.2'
-gem 'stomp', '~> 1.2.6'
+gem 'stomp', '~> 1.2.7'
 gem 'jquery-rails','2.1.3'
 gem "dynamoid", :git => 'git://github.com/indykish/Dynamoid.git'
 gem "json", '~> 1.7.5'
+gem 'fake_dynamo', '~> 0.1.0'
 
 
 # To use ActiveModel has_secure_password
 gem 'bcrypt-ruby', :require => 'bcrypt'
+gem 'less-rails', '~> 2.2.6' #for using mailer. Without this shows error as method missing 'less'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
+  gem 'twitter-bootstrap-rails'
   gem 'sass-rails','3.2.5'
   gem 'coffee-rails','3.2.2'
 
@@ -37,11 +40,11 @@ group :assets do
   # gem 'therubyracer'
 
   gem 'uglifier', '>= 1.3.0'
-  gem 'twitter-bootstrap-rails'
+ # gem 'twitter-bootstrap-rails'
 end
 
 group :test do
-  gem 'capybara','1.1.2'
+  gem 'capybara','1.1.3'
   gem 'factory_girl_rails'
   gem 'cucumber-rails', :require => false
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,8 @@ class UsersController < ApplicationController
   end
 
   def show
-
-    @user = User.find(params[:id])
-    @connector_project = ConnectorProject.all
+   @user = User.find(params[:id])
+    #@connector_project = ConnectorProject.all
     if !@user.organization
       flash[:error] = "Hey stranger. we need details about your work. Please update your work details to proceed further."
       redirect_to edit_user_path(current_user)
@@ -27,12 +26,38 @@ class UsersController < ApplicationController
 
   end
 
+
+ def email_verify
+	@user= User.find_by_verification_hash(params[:format])
+	logger.debug "params befor mailer = #{params}"
+	
+	logger.debug "@user befor mailer = #{@user}"
+	UserMailer.welcome_email(@user).deliver
+	logger.debug "@user = #{@user}"
+ end
+
+  def verified_email
+	@user= User.find_by_verification_hash(params[:format])
+	logger.debug "@user #{@user.to_yaml}"
+	if @user.verification_hash === params[:format]
+	@user.update_attribute(:verified_email, 'true')
+	#sign_in @user
+	flash[:alert] = "Welcome back #{@user.first_name}. Your email #{@user.email} is verified. Thank you."
+        redirect_to signin_path(@user)
+	else
+		logger.debug "Wrong user"
+		flash[:alert] = "Sorry wrong verification"
+		redirect_to sign_up_path
+	end	
+  end
+
+
   def create
     @user = User.new(params[:user])
 
     if @user.save
       sign_in @user
-      flash[:alert] = "Welcome back #{current_user.first_name}"
+      flash[:alert] = "Welcome #{current_user.first_name}"
       redirect_to customizations_show_url
     else
       render 'new'
@@ -47,6 +72,8 @@ class UsersController < ApplicationController
 
   end
 
+ 
+
   def update
 
     @user=User.find(params[:id])
@@ -54,12 +81,16 @@ class UsersController < ApplicationController
 
     if @user.update_attributes(params[:user])
       logger.debug "user-update"
+      random_token = p SecureRandom.urlsafe_base64(nil, true)
+      @user.organization.update_attribute(:api_token, random_token)
 
-      flash[:alert] = "Hi #{current_user.first_name},Your profile was updated Successfully"
+      flash[:alert] = "Hi #{current_user.first_name},Your profile was updated Successfully and your API token is 
+#{random_token}"
       sign_in @user
-
+	
+	logger.debug "show org #{@user.to_yaml}"
       redirect_to @user
-
+	
     else
       render 'edit'
     end
