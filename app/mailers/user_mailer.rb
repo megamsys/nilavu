07d6c68ec75f40b79cc45768d371bb9e330bade7
thomasalrin <alrin@megam.co.in>
2013-05-30 17:26:04 +0530

    update on 30-5-13

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,7 @@ class UserMailer < ActionMailer::Base
     logger.debug "user = #{user}"
     @mailer_user = user
     @random_token = user.verification_hash
-    #@url  = "http://localhost:3000/verified_email.#{@random_token}"
-    @url  = "http://thawing-woodland-9218.herokuapp.com//verified_email.#{@random_token}"
+    @url  = "http://localhost:3000/verified_email.#{@random_token}"
     mail(:to => user.email, :subject => "Megam Account Confirmation")
   end
 
