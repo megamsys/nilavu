3033d6c8232c487ca6258a78dfa2539c938cd270
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-07-26 15:02:10 +0530

    Minor update (comment resque)

--- app/controllers/cloud_books_controller.rb
+++ app/controllers/cloud_books_controller.rb
@@ -2,7 +2,6 @@ class CloudBooksController < ApplicationController
 
   respond_to :html, :js
   add_breadcrumb "Home", :root_path
-  
   def new
     @book =  current_user.cloud_books.build
     add_breadcrumb "cloud_book_create", cloud_book_create_path
@@ -12,11 +11,10 @@ class CloudBooksController < ApplicationController
   def create
     sleep 2
     @book = current_user.cloud_books.create(params[:cloud_book])
-    if @book.save         
+    if @book.save
       nodes = Hash["predefname" => "java", "predefcloudname" => "ec2_java"]
-      success = Resque.enqueue(APINodes, nodes)
-     #redirect_to cloud_books_success_form_url(:predef_name => @book.predef_name, :predef_cloud_name => @book.predef_cloud_name ), :gflash => { :success => { :value => "Welcome #{current_user.first_name}. Your cloud book(predef) was created successfully.", :sticky => false, :nodom_wrap => true } }
-     
+      #   success = Resque.enqueue(APINodes, nodes)
+      redirect_to cloud_books_success_form_url(:predef_name => @book.predef_name, :predef_cloud_name => @book.predef_cloud_name ), :gflash => { :success => { :value => "Welcome #{current_user.first_name}. Your cloud book(predef) was created successfully.", :sticky => false, :nodom_wrap => true } }
     else
       render 'new'
     end
