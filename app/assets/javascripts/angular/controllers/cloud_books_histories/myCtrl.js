d7ee68f3493414e09e6241211cc72b5672a10beb
rajthilakmca <rajthilak@megam.co.in>
2013-08-23 19:16:16 +0530

    Succefully completed cloud_book_histories

--- app/assets/javascripts/angular/controllers/cloud_books_histories/myCtrl.js
+++ app/assets/javascripts/angular/controllers/cloud_books_histories/myCtrl.js
@@ -1,11 +1,39 @@
-function MyCtrl($scope, socket) {
-    socket.on('message', function(data) {
-       
-    });
+function MyCtrl($scope, socket) {	
+	$scope.logs = [];
+	$scope.logerrors = [];
+	$scope.sendmessage = function(data) {
+		alert("connect");
+		socket.emit('message', data)
+	};
 
-    $scope.$on('$destroy', function (event) {
-        socket.removeAllListeners();
-        // or something like
-        // socket.removeListener(this);
-    });
+	socket.on('connect', function(data) {
+
+	});
+
+	socket.on('disconnect', function(event) {
+		socket.removeAllListeners();
+		alert("disconnect");	
+	});
+
+	socket.on('message', function(message) {	
+		var replaceChar = message.replace(/\@/g, "");
+		var parse = JSON.parse(replaceChar);		
+		$scope.source = parse.source;					
+		$scope.logs.push(parse);
+		$scope.predicate = 'timestamp';		
+		
+		/*if((parse.message).indexOf("INFO") == 0){
+			$scope.mycolor = 'log_info';				
+		}
+		if((parse.message).indexOf("ERROR") == 0){
+			$scope.mycolor = 'log_error';			
+		}
+		/*if((parse.message).indexOf("WARN") == 0){
+			$scope.mycolor = 'log_warn';
+		}
+		if((parse.message).indexOf("INFO") && (parse.message).indexOf("ERROR") && (parse.message).indexOf("WARN") != 0){
+			$scope.mycolor = 'log_info';
+		}		*/
+	});
 };
+
---
+++ app/assets/javascripts/angular/filters/log.js
@@ -0,0 +1,6 @@
+app.filter('split', function() {
+        return function(input, splitChar, splitIndex) {
+            // do some bounds checking here to ensure it has that index
+            return input.split(splitChar)[splitIndex];
+        }
+    });
