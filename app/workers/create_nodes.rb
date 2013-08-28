b3120a15b15a3decbd6dfadc5ff158eca3d8a9f4
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-08-28 17:42:29 +0530

    Added the youtube video link

--- app/workers/create_nodes.rb
+++ app/workers/create_nodes.rb
@@ -1,33 +1,31 @@
 class CreateNodes
-
-  
   #this returns a Megam::Account object
   def self.perform(new_node)
-   begin
-    Megam::Config[:email] = new_node[:email]
-    Megam::Config[:api_key] = new_node[:api_key]
-     @excon_res = Megam::Node.create(new_node[:node])
-     rescue ArgumentError => ae
-	puts "===========================> AE <======================================="
-	hash = {"msg" => ae.message, "msg_type" => "error"}
-	re = Megam::Error.from_hash(hash)
-	@res = {"data" => {:body => re}}
-	return @res["data"][:body]
-=begin
-     rescue Megam::API::Errors::ErrorWithResponse => ewr
-	puts "===========================> EWR <======================================="
-	hash = {"msg" => ewr.message, "msg_type" => "error"}
-	re = Megam::Error.from_hash(hash)
-	@res = {"data" => {:body => re}}
-	return @res["data"][:body]
-     rescue StandardError => se
-	puts "===========================> SE <======================================="
-	hash = {"msg" => se.message, "msg_type" => "error"}
-	re = Megam::Error.from_hash(hash)
-	@res = {"data" => {:body => re}}
-	return @res["data"][:body]
-=end
-   end
-	@excon_res.data[:body]
+    begin
+      Megam::Config[:email] = new_node[:email]
+      Megam::Config[:api_key] = new_node[:api_key]
+      @excon_res = Megam::Node.create(new_node[:node])
+    rescue ArgumentError => ae
+      puts "===========================> AE <======================================="
+      hash = {"msg" => ae.message, "msg_type" => "error"}
+      re = Megam::Error.from_hash(hash)
+      @res = {"data" => {:body => re}}
+      return @res["data"][:body]
+    =begin
+    rescue Megam::API::Errors::ErrorWithResponse => ewr
+    puts "===========================> EWR <======================================="
+    hash = {"msg" => ewr.message, "msg_type" => "error"}
+    re = Megam::Error.from_hash(hash)
+    @res = {"data" => {:body => re}}
+    return @res["data"][:body]
+    rescue StandardError => se
+    puts "===========================> SE <======================================="
+    hash = {"msg" => se.message, "msg_type" => "error"}
+    re = Megam::Error.from_hash(hash)
+    @res = {"data" => {:body => re}}
+    return @res["data"][:body]
+    =end
+    end
+    @excon_res.data[:body]
   end
 end
