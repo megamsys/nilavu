e806d46592b358ee1428b81645942e7dc8cb5581
thomasalrin <alrin@megam.co.in>
2013-09-27 19:00:47 +0530

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,10 +49,6 @@
 //= require angular/templates/widget/show.jst
 //= require angular/templates/widgets/pernode/show.jst
 //= require angular/templates/widget/book_show.jst
-//= require mootools
-//= require swfobject
-//= require videobox
-//= require tour
 //= require jquery.peity
 //= require underscore
 //= require angular
