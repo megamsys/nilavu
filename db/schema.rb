d40f9de1ae8220f07d66689fc759dee6406e2e4d
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-10-23 10:57:43 +0530

    did a dbinit, no changes done to schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130828101201) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "apps_items", force: true do |t|
     t.integer  "users_id"
     t.integer  "cloud_identity_id"
