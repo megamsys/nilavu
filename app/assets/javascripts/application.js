b571b9dd45e35d4f6aa0c342f31dd17eb18a1590
thomasalrin <alrin@megam.co.in>
2013-10-23 12:43:32 +0530

    DB next btn enable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,6 +37,7 @@
 //= require jquery.wizard
 //= require jquery.peity
 //= require cloud_books
+//= require predef_clouds
 //= require connector_projects
 //= require unicorn.form_validation
 //= require unicorn.wizard
--- app/assets/javascripts/predef_cloud.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){		
-	$("#new_db1 input:radio").click(function() {
-		$("#db_next").attr("disabled", false); //enable next button
-	});
-});
