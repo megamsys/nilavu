c61e55538f6e8d4769af7d1e2ca3e1e0c6b688ec
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-11-20 11:28:13 +0530

    Just a Source > Format of code

--- app/controllers/cloud_books_controller.rb
+++ app/controllers/cloud_books_controller.rb
@@ -6,20 +6,20 @@ class CloudBooksController < ApplicationController
     if current_user.cloud_books.any?
       add_breadcrumb "Cloud_books", cloud_books_path
       @cloud_books = current_user.cloud_books
-	logger.debug "Cloud Book Index ==> "
+      logger.debug "Cloud Book Index ==> "
     else
       redirect_to new_cloud_book_path
     end
   end
 
   def definition
-      add_breadcrumb "Cloud_books", cloud_books_path
-      add_breadcrumb "Cloud_book's Definition", definition_path(params['format'])
-      @cloud_books = current_user.cloud_books
-	logger.debug "Cloud Book Definition ==> "
-	puts params['format']
+    add_breadcrumb "Cloud_books", cloud_books_path
+    add_breadcrumb "Cloud_book's Definition", definition_path(params['format'])
+    @cloud_books = current_user.cloud_books
+    logger.debug "Cloud Book Definition ==> "
+    puts params['format']
     @book = CloudBook.find(params['format'])
-	puts @book.inspect
+    puts @book.inspect
   end
 
   def new
@@ -50,14 +50,14 @@ class CloudBooksController < ApplicationController
     else
     #if @predef_cloud.some_msg[:msg_type] != "error"
       pred = FindPredefsByName.perform(predef_options)
-	puts "=================================> Predef COLLECTION <=============================================="
-	puts pred.inspect
+      puts "=================================> Predef COLLECTION <=============================================="
+      puts pred.inspect
       if pred.class == Megam::Error
         redirect_to new_cloud_book_path, :gflash => { :warning => { :value => "#{pred.some_msg[:msg]}", :sticky => false, :nodom_wrap => true } }
       else
         @predef = pred.lookup(@predef_name)
         @domain_name = ".megam.co"
-	@no_of_instances=params[:no_of_instances]
+        @no_of_instances=params[:no_of_instances]
       end
     end
   #else
@@ -71,7 +71,7 @@ class CloudBooksController < ApplicationController
   def create
     options = { :email => current_user.email, :api_key => current_user.api_token, :node => mk_node(params, "server", "create") }
     @node = CreateNodes.perform(options)
-	puts "==================> @NODE in CB CREATE == > #{@node.inspect}"
+    puts "==================> @NODE in CB CREATE == > #{@node.inspect}"
     if @node.class == Megam::Error
       @res_msg="Sorry Something Wrong. Please contact #{ActionController::Base.helpers.link_to 'Our Support !.', "http://support.megam.co/"}."
       respond_to do |format|
@@ -81,34 +81,34 @@ class CloudBooksController < ApplicationController
       end
     #redirect_to new_cloud_book_path, :gflash => { :warning => { :value => "#{@node.some_msg[:msg]}", :sticky => false, :nodom_wrap => true } }
     else
-	#i = 0
-	no_of_nodes = params[:no_of_instances].to_i
-for i in 1..no_of_nodes
- if current_user.cloud_books.count < 2
-	params[:cloud_book][:name]="#{params[:cloud_book][:name]}#{i}"
-      @book = current_user.cloud_books.create(params[:cloud_book])
-      @book.save
+    #i = 0
+      no_of_nodes = params[:no_of_instances].to_i
+      for i in 1..no_of_nodes
+        if current_user.cloud_books.count < 2
+          params[:cloud_book][:name]="#{params[:cloud_book][:name]}#{i}"
+          @book = current_user.cloud_books.create(params[:cloud_book])
+          @book.save
 =begin
-      if @node.request["req_id"]
-        params = {:book_name => "#{@book.name}#{@book.domain_name}", :request_id => @node.request["req_id"], :status => @node.status}
-      else
+if @node.request["req_id"]
+params = {:book_name => "#{@book.name}#{@book.domain_name}", :request_id => @node.request["req_id"], :status => @node.status}
+else
 =end
-        params = {:book_name => "#{@book.name}#{@book.domain_name}", :request_id => "req_id", :status => "status"}
- #end
+          params = {:book_name => "#{@book.name}#{@book.domain_name}", :request_id => "req_id", :status => "status"}
+          #end
 
-      @history = @book.cloud_books_histories.create(params)
-    @history.save
- end
-end
+          @history = @book.cloud_books_histories.create(params)
+        @history.save
+        end
+      end
     end
   end
 
   def show
-puts params
+    puts params
     @book = CloudBook.find(params[:id])
-	get_node = { :email => current_user.email, :api_key => current_user.api_token, :node => "#{@book.name}#{@book.domain_name}" }
-	@node = FindNodeByName.perform(get_node)
-	logger.debug "Get Node By Name NODE ==> "
+    get_node = { :email => current_user.email, :api_key => current_user.api_token, :node => "#{@book.name}#{@book.domain_name}" }
+    @node = FindNodeByName.perform(get_node)
+    logger.debug "Get Node By Name NODE ==> "
 
     if @node.class == Megam::Error
       @res_msg="Sorry Something Wrong. Please contact #{ActionController::Base.helpers.link_to 'Our Support !.', "http://support.megam.co/"}."
@@ -119,30 +119,29 @@ puts params
       end
     #redirect_to new_cloud_book_path, :gflash => { :warning => { :value => "#{@node.some_msg[:msg]}", :sticky => false, :nodom_wrap => true } }
     else
-	@cloud_book = @node.lookup("#{@book.name}#{@book.domain_name}")
-	if params["action_name"]
-		@full_detail = true 
-		@requests = "TEST"
-      respond_to do |format|
-        format.js {
-          respond_with(@full_detail, @cloud_book, @requests, :layout => !request.xhr? )
-        }
+      @cloud_book = @node.lookup("#{@book.name}#{@book.domain_name}")
+      if params["action_name"]
+        @full_detail = true
+        @requests = "TEST"
+        respond_to do |format|
+          format.js {
+            respond_with(@full_detail, @cloud_book, @requests, :layout => !request.xhr? )
+          }
+        end
+      else
+        respond_to do |format|
+          format.js {
+            respond_with(@cloud_book, :layout => !request.xhr? )
+          }
+        end
       end
-	else
-      respond_to do |format|
-        format.js {
-          respond_with(@cloud_book, :layout => !request.xhr? )
-        }
-      end
-	end
     end
   end
 
   def destroy
     @book = CloudBook.find(params[:id])
 
-
-@com = {
+    @com = {
 "systemprovider" => {
 "provider" => {
 "prov" => "chef"
@@ -171,16 +170,15 @@ puts params
 }
 }
 
-        node_hash = {
-          "node_name" => "#{@book.name}#{@book.domain_name}",
-	"req_type" => "delete",
-          "command" => @com
-        }
-
+    node_hash = {
+      "node_name" => "#{@book.name}#{@book.domain_name}",
+      "req_type" => "delete",
+      "command" => @com
+    }
 
     options = { :email => current_user.email, :api_key => current_user.api_token, :req => node_hash }
     @node = CreateRequests.perform(options)
-	puts "==================> @NODE in CB CREATE == > #{@node.inspect}"
+    puts "==================> @NODE in CB CREATE == > #{@node.inspect}"
     if @node.class == Megam::Error
       @res_msg="Sorry Something Wrong. Please contact #{ActionController::Base.helpers.link_to 'Our Support !.', "http://support.megam.co/"}."
       respond_to do |format|
@@ -189,16 +187,14 @@ puts params
         }
       end
     else
-	@book.cloud_books_histories.each do |cbh|
-		cbh.destroy
-	end
-	@book.destroy
-	redirect_to cloud_books_path, :gflash => { :success => { :value => "Cloud book and its histories are deleted successfully", :sticky => false, :nodom_wrap => true } }
+      @book.cloud_books_histories.each do |cbh|
+        cbh.destroy
+      end
+      @book.destroy
+      redirect_to cloud_books_path, :gflash => { :success => { :value => "Cloud book and its histories are deleted successfully", :sticky => false, :nodom_wrap => true } }
 
     end
   end
-
-
 
   private
 
@@ -212,26 +208,26 @@ puts params
       else
       logger.debug "Make command for chef run"
 
-        node_hash = {
-          "node_name" => "#{data[:cloud_book][:name]}#{data[:cloud_book][:domain_name]}",
-	"node_type" => "#{data[:cloud_book][:book_type]}",
-	"req_type" => "#{action}",
-	"noofinstances" => data[:no_of_instances],
-          "command" => command,
-          "predefs" => {"name" => data[:predef][:name], "scm" => data[:deps_scm],
-            "db" => "postgres@postgresql1.megam.com/morning.megam.co", "war" => data[:deps_war], "queue" => "queue@queue1", "runtime_exec" => data[:predef][:runtime_exec]},
-	"appdefns" => {"timetokill" => "", "metered" => "", "logging" => "", "runtime_exec" => ""},
-	"boltdefns" => {"username" => "", "apikey" => "", "store_name" => "", "url" => "", "prime" => "", "timetokill" => "", "metered" => "", "logging" => "", "runtime_exec" => ""},
-	"appreq" => {},
-	"boltreq" => {}
-        }
+      node_hash = {
+        "node_name" => "#{data[:cloud_book][:name]}#{data[:cloud_book][:domain_name]}",
+        "node_type" => "#{data[:cloud_book][:book_type]}",
+        "req_type" => "#{action}",
+        "noofinstances" => data[:no_of_instances],
+        "command" => command,
+        "predefs" => {"name" => data[:predef][:name], "scm" => data[:deps_scm],
+          "db" => "postgres@postgresql1.megam.com/morning.megam.co", "war" => data[:deps_war], "queue" => "queue@queue1", "runtime_exec" => data[:predef][:runtime_exec]},
+        "appdefns" => {"timetokill" => "", "metered" => "", "logging" => "", "runtime_exec" => ""},
+        "boltdefns" => {"username" => "", "apikey" => "", "store_name" => "", "url" => "", "prime" => "", "timetokill" => "", "metered" => "", "logging" => "", "runtime_exec" => ""},
+        "appreq" => {},
+        "boltreq" => {}
+      }
 
-	if data[:cloud_book][:book_type] == "APP"
-		node_hash["appdefns"] = {"timetokill" => "0", "metered" => "megam", "logging" => "megam", "runtime_exec" => "#{data['runtime_exec']}"}
-	end
-	if data[:cloud_book][:book_type] == "BOLT"
-		node_hash["boltdefns"] = {"username" => "#{data['user_name']}", "apikey" => "#{data['password']}", "store_name" => "#{data['store_db_name']}", "url" => "#{data['url']}", "prime" => "#{data['prime']}", "timetokill" => "", "metered" => "", "logging" => "", "runtime_exec" => "#{data['runtime_exec']}" }
-	end
+      if data[:cloud_book][:book_type] == "APP"
+        node_hash["appdefns"] = {"timetokill" => "0", "metered" => "megam", "logging" => "megam", "runtime_exec" => "#{data['runtime_exec']}"}
+      end
+      if data[:cloud_book][:book_type] == "BOLT"
+        node_hash["boltdefns"] = {"username" => "#{data['user_name']}", "apikey" => "#{data['password']}", "store_name" => "#{data['store_db_name']}", "url" => "#{data['url']}", "prime" => "#{data['prime']}", "timetokill" => "", "metered" => "", "logging" => "", "runtime_exec" => "#{data['runtime_exec']}" }
+      end
     end
     logger.debug "COMMAND HASH ==> #{node_hash.inspect}"
 
