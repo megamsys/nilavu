72510704a6410e8fce43154db031d4e184c7fcb2
rajthilakmca <rajthilak@megam.co.in>
2013-11-26 19:37:19 +0530

    fix cross clouds options

--- app/assets/javascripts/cross_clouds.js
+++ app/assets/javascripts/cross_clouds.js
@@ -39,11 +39,31 @@ $(document).ready(function() {
 		switch (cc) {
 		case "Amazon EC2":			
 			$("<div class='offset2' id='cloud_logo'><img src='/assets/logo_aws.png' height='100' width='100'/></div>").replaceAll('#cloud_logo');
-			$("#cross_cloud_name").val("aws");			
+			$("#cross_cloud_name").val("aws");	
+			$("#cc_name").show();
+			$("#cc_group").show();
+			$("#cc_image").show();
+			$("#cc_flavor").show();
+			$("#cc_sshkey").show();
+			$("#cc_sshuser").show();
+			$("#cc_awspk").show();
+			$("#cc_rsapk").show();
+			$("#cc_awsak").show();
+			$("#cc_awssk").show();
 			break;
 		case "hp cloud":		
 			$("<div class='offset2' id='cloud_logo'><img src='/assets/logo_hp.png' height='100' width='100'/></div>").replaceAll('#cloud_logo');
-			$("#cross_cloud_name").val("hp");						
+			$("#cross_cloud_name").val("hp");	
+			$("#cc_name").show();
+			$("#cc_group").show();
+			$("#cc_image").show();
+			$("#cc_flavor").show();
+			$("#cc_sshkey").show();
+			$("#cc_sshuser").show();
+			$("#cc_awspk").show();
+			$("#cc_rsapk").show();
+			$("#cc_awsak").show();
+			$("#cc_awssk").show();
 			break;
 		case "Google cloud Engine":			
 			$('#google_auth').prop("disabled",false);			
@@ -51,11 +71,31 @@ $(document).ready(function() {
 			   "<a href='/auth/google_oauth2'  target='_self'><img src='/assets/signin_google.png' /></a></br>" +
 					"<img src='/assets/logo_gce.png' height='100' width='100'/>" +
 					"</div>").replaceAll('#cloud_logo');
-			$("#cross_cloud_name").val("google");			;
+			$("#cross_cloud_name").val("google");		
+			$("#cc_name").show();
+			$("#cc_group").toggle();
+			$("#cc_image").show();
+			$("#cc_flavor").show();
+			$("#cc_sshkey").toggle();
+			$("#cc_sshuser").show();
+			$("#cc_awspk").toggle();
+			$("#cc_rsapk").show();
+			$("#cc_awsak").toggle();
+			$("#cc_awssk").toggle();
 			break;
 		default:			
 			$("<div class='offset2' id='cloud_logo'><img src='/assets/logo_aws.png' height='100' width='100'/></div>").replaceAll('#cloud_logo');
-		    $("#cross_cloud_name").val("aws");		    
+		    $("#cross_cloud_name").val("aws");
+		    $("#cc_name").show();
+			$("#cc_group").show();
+			$("#cc_image").show();
+			$("#cc_flavor").show();
+			$("#cc_sshkey").show();
+			$("#cc_sshuser").show();
+			$("#cc_awspk").show();
+			$("#cc_rsapk").show();
+			$("#cc_awsak").show();
+			$("#cc_awssk").show();
 		    break;
 		}
 		
