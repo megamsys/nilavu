a9b294351b1f3bfefb94e0c764b09059701e19a0
Thomas Alrin <alrin@megam.co.in>
2013-12-18 17:34:10 +0530

    production change for angular

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,12 +15,13 @@ Cloudauth::Application.configure do
   GA.script_source = "('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(:mangle => false)
 
   # Compress JavaScripts and CSS
-  #config.assets.compress = true
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -78,3 +79,4 @@ Cloudauth::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+
