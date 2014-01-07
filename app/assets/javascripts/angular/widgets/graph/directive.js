78053a7c2094b6a0eac4d2dcbe4435fcfbca771d
rajthilakmca <rajthilak@megam.co.in>
2014-01-07 20:56:37 +0530

    Fix profile, signin, signout pages and icons

--- app/assets/javascripts/angular/widgets/graph/directive.js
+++ app/assets/javascripts/angular/widgets/graph/directive.js
@@ -5,7 +5,7 @@ app.directive("graph", ["FlotrGraphHelper", "GraphModel", "$routeParams", "Sourc
   var linkFn = function(scope, element, attrs) {  	 
 	  
 	  function onSuccess(data) {
-		  
+		     console.log("-----------",data);
 		     scope.uptime_data=parseUptime(data);
 		     scope.rpm_data=parseRPM(data);
 	    	 var plot = $.plot("#graph_placeholder", FlotrGraphHelper.transformSeriesOfDatapoints(data, scope.widget, currentColors), FlotrGraphHelper.defaultOptions(scope.widget, 0.7));
