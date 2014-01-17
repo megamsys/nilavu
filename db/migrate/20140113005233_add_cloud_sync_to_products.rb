9806b46b3bd3c08aa1a4bebc38912a32ab849d78
Kishorekumar Neelamegam <nkishore@megam.co.in>
2014-01-17 18:45:00 +0530

    migration

--- db/migrate/20140113005233_add_cloud_sync_to_products.rb
+++ db/migrate/20140113005233_add_cloud_sync_to_products.rb
@@ -2,4 +2,7 @@ class AddCloudSyncToProducts < ActiveRecord::Migration
   def change
     add_column :products, :cloud_sync, :boolean
   end
+  def self.down
+    remove_column :products, :cloud_sync, :boolean
+  end 
 end
