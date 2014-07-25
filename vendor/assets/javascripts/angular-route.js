3437469b23bc560448392baff4f945486b3c7349
Kishorekumar Neelamegam <nkishore@megam.co.in>
2014-07-25 10:52:03 +0530

    Moving angularjs to 1.3beta14, as beta 15,beta16 aren't usable.

--- vendor/assets/javascripts/angular-route.js
+++ vendor/assets/javascripts/angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-beta.16
+ * @license AngularJS v1.3.0-beta.14
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -473,7 +473,9 @@ function $RouteProvider(){
       for (var i = 1, len = m.length; i < len; ++i) {
         var key = keys[i - 1];
 
-        var val = m[i];
+        var val = 'string' == typeof m[i]
+              ? decodeURIComponent(m[i])
+              : m[i];
 
         if (key && val) {
           params[key.name] = val;
