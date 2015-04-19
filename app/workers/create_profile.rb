594eb900d5df995d13ba55cbb3503287bd5bef9a
Yeshwanth <morpheyesh@gmail.com>
2015-04-19 23:59:03 +0530

    profile API fix

---
+++ app/workers/create_profile.rb
@@ -0,0 +1,24 @@
+class CreateProfile
+  Rails.logger.debug "Worker: " "CreateProfile"
+  def self.perform(pro, tmp_email, tmp_apikey)
+    begin
+      @excon_res = Megam::Profile.create(pro, tmp_email, tmp_apikey)
+    rescue ArgumentError => ae
+      hash = {"msg" => ae.message, "msg_type" => "error"}
+      re = Megam::Error.from_hash(hash)
+      @res = {"data" => {:body => re}}
+      return @res["data"][:body]
+    rescue Megam::API::Errors::ErrorWithResponse => ewr
+      hash = {"msg" => ewr.message, "msg_type" => "error"}
+      re = Megam::Error.from_hash(hash)
+      @res = {"data" => {:body => re}}
+      return @res["data"][:body]
+    rescue StandardError => se
+      hash = {"msg" => se.message, "msg_type" => "error"}
+      re = Megam::Error.from_hash(hash)
+      @res = {"data" => {:body => re}}
+      return @res["data"][:body]
+    end
+    @excon_res.data[:body]
+  end
+end
