0f1c46d06dab2f53d17764eacc5b5ab5c47f6c2e
Rajthilak <rajthilak@megam.io>
2015-05-19 11:58:32 +0530

    Fix monitoring route

--- app/assets/javascripts/angular/controllers/dashboard_show_ctrl.js
+++ app/assets/javascripts/angular/controllers/dashboard_show_ctrl.js
@@ -1,66 +1,58 @@
-/* 
-** Copyright [2013-2015] [Megam Systems]
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-** http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-*/
-app.controller("DashboardShowCtrl", ["$scope", "$rootScope", "$routeParams", "$location", "$timeout", "$window", "Dashboard", "Widget", "UpdateWidget", "Sources", function($scope, $rootScope, $routeParams, $location, $timeout, $window, Dashboard, Widget, UpdateWidget, Sources) {
-	
-  console.log("----------entry--");
-  $scope.bookwidget = "widgetpernode";
-  $rootScope.resolved = false;   
-  $scope.dashboard_id = $routeParams.id; 
-  
- // $scope.dashboard = Dashboard.get({ id: $routeParams.id });
- $scope.widgets   = Widget.query({ dashboard_id: 1 }, function() {	  
-   $rootScope.resolved = true;
-  }); 
-  
-  
-  $scope.$on('handleBroadcast', function() {	  
-      $scope.widget = UpdateWidget.message;      
-      replaceWidget($scope.widget.id, $scope.widget);    
-      $scope.sm = UpdateWidget.sm;
-  });       
-  
-  $scope.c_launch_namegen = $routeParams.book;
-  
-  function replaceWidget(id, widget) {    
-	    var w = _.findWhere($scope.widgets, { id: widget.id });
-	    _.extend(w, widget);
-	  } 
-  
-	//$scope.a_books = availableBooks();
-	
-	function bookMapping(book) {        
+/*
+ ** Copyright [2013-2015] [Megam Systems]
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ ** http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+app.controller("DashboardShowCtrl", ["$scope", "$rootScope", "$routeParams", "$location", "$timeout", "$window", "Widget", "UpdateWidget", "Sources",
+function($scope, $rootScope, $routeParams, $location, $timeout, $window, Widget, UpdateWidget, Sources) {
+
+	$scope.bookwidget = "widgetpernode";
+	$rootScope.resolved = false;
+	$scope.dashboard_id = $routeParams.id;
+
+	$scope.widgets = Widget.query({
+		dashboard_id : 1
+	}, function() {
+		$rootScope.resolved = true;
+	});
+
+	$scope.$on('handleBroadcast', function() {
+		$scope.widget = UpdateWidget.message;
+		replaceWidget($scope.widget.id, $scope.widget);
+		$scope.sm = UpdateWidget.sm;
+	});
+
+	$scope.c_launch_namegen = $routeParams.book;
+
+	function replaceWidget(id, widget) {
+		var w = _.findWhere($scope.widgets, {
+			id : widget.id
+		});
+		_.extend(w, widget);
+	}
+
+	function bookMapping(book) {
 		return {
-          value: book     
-        };
-      }
-     
-    /*  function availableBooks() {    	  
-        var a_books = $.Books.books;    		  
-        return _.compact(_.map(a_books, function(book) {      
-        	return bookMapping(book);
-        }));	        
-      }    */
-     
-      $scope.sm_view = function() {    	 
-    	  UpdateWidget.showForBroadcast(false);    		  
-      }
-      
-      if ($routeParams.book != null) {    	  
-    	  $scope.widgetpernode = "widgetpernode";
-    	  $scope.templateUrl = "angular/templates/widget/book_show.html.erb";
-      }
-  
+			value : book
+		};
+	}
+
+	$scope.sm_view = function() {
+		UpdateWidget.showForBroadcast(false);
+	}
+	if ($routeParams.book != null) {
+		$scope.widgetpernode = "widgetpernode";
+		$scope.templateUrl = "angular/templates/widget/book_show.html.erb";
+	}
+
 }]);
--- app/controllers/api/widgets_controller.rb
+++ app/controllers/api/widgets_controller.rb
@@ -16,45 +16,9 @@
 module Api
   class WidgetsController < ApplicationController
     respond_to :json
-    def show
-      widget = Widget.for_dashboard(params[:dashboard_id]).find(params[:id])
-      respond_with(widget)
-    end
-
     def index
-      wid = [{"Widget id" => 1, "name" => "graph", "kind" => "datapoints", "size" => nil, "source" => "ganglia", "targets" => ["cpu_system"], "range" => "hour", "dashboard_id" => 1, "widget_type" => "pernode", "created_at" => nil, "updated_at" => nil}]
+      wid = [{"Widget id" => 1, "name" => "graph", "kind" => "datapoints", "size" => nil, "source" => "demo", "targets" => ["cpu_system"], "range" => "hour", "dashboard_id" => 1, "widget_type" => "pernode", "created_at" => nil, "updated_at" => nil}]
       respond_with(wid)
-    end
-
-    def create
-      logger.debug ("> api.widgets: create")
-      dashboard = Dashboard.find(params[:dashboard_id])
-      input = JSON.parse(request.body.read.to_s)
-      widget = dashboard.widgets.build(Widget.slice_attributes(input))
-      if widget.save
-        render :json => widget, :status => :created, :location => api_dashboard_widget_url(:dashboard_id => dashboard.id, :id => widget.id)
-      else
-        render :json => widget.errors, :status => :unprocessable_entity
-      end
-    end
-
-    def update
-      logger.debug ("> api.widgets: update")
-      dashboard = Dashboard.find(params[:dashboard_id])
-      widget = dashboard.widgets.find(params[:id])
-      input = JSON.parse(request.body.read.to_s)
-      if widget.update_attributes(Widget.slice_attributes(input))
-        head :no_content
-      else
-        render :json => widget.errors, :status => :unprocessable_entity
-      end
-    end
-
-    def destroy
-      dashboard = Dashboard.find(params[:dashboard_id])
-      widget = dashboard.widgets.find(params[:id])
-      widget.destroy
-      head :no_content
     end
 
   end
