bcd5e07df18bc4a61f529371f2d620e187635954
vijaykanthm28 <mvijykanth@megam.io>
2015-09-23 12:49:07 +0530

    test file removed

---
+++ app/assets/javascripts/components/storages.js.jsx
@@ -0,0 +1,13 @@
+var HelloMessage = React.createClass({
+
+render: function() {
+  return (
+    <div>
+      <div>Title:hello megam welcome  </div>
+      <div>Body: </div>
+      <div>Published: </div>
+      <div>Published By: </div>
+    </div>
+  );
+  }
+});
