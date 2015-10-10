7003b4e48a469820ac1b8c6907868b1acbdd87b3
indykish <nkishore@megam.io>
2015-10-10 13:32:34 +0530

    beautify, and stick_keys loops to stick_host, stick_storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,11 +22,10 @@ class ApplicationController < ActionController::Base
   around_action :catch_exceptions
   before_filter :set_user_language
 
-  #for internationalization
+  # for internationalization
 
   def set_user_language
-    I18n.locale= 'en'
-
+    I18n.locale = 'en'
   end
 
   # a catcher exists using rails globber for routes in config/application.rb to trap 404.
@@ -45,13 +44,13 @@ class ApplicationController < ActionController::Base
   def render_404(exception = nil)
     @not_found_path = exception.message if exception
     if !signed_in?
-     gflash :error => "#{exception}"
-     redirect_to signin_path, flash: { error: 'You must first sign in or sign up.' }
+      gflash error: "#{exception}"
+      redirect_to signin_path, flash: { error: 'You must first sign in or sign up.' }
     else
-     respond_to do |format|
-      format.html { render template: 'errors/not_found', layout: 'application', status: 404 }
-      format.all { render nothing: true, status: 404 }
-    end
+      respond_to do |format|
+        format.html { render template: 'errors/not_found', layout: 'application', status: 404 }
+        format.all { render nothing: true, status: 404 }
+      end
   end
   end
 
@@ -60,16 +59,16 @@ class ApplicationController < ActionController::Base
   # usage way.
   def render_500(exception = nil)
     puts_stacktrace(exception) if exception
-      if !signed_in?
-     gflash :error => "#{exception}"
-     redirect_to signin_path, flash: { error: 'You must first sign in or sign up.' }
+    if !signed_in?
+      gflash error: "#{exception}"
+      redirect_to signin_path, flash: { error: 'You must first sign in or sign up.' }
     else
-     respond_to do |format|
-       format.html { render template: 'errors/internal_server_error', layout: 'application', status: 500 }
-       format.js { render template: 'errors/internal_server_error', layout: 'application', status: 500 }
-       format.all { render nothing: true, status: 500 }
-     end
-    end
+      respond_to do |format|
+        format.html { render template: 'errors/internal_server_error', layout: 'application', status: 500 }
+        format.js { render template: 'errors/internal_server_error', layout: 'application', status: 500 }
+        format.all { render nothing: true, status: 500 }
+      end
+  end
   end
 
   private
@@ -87,7 +86,7 @@ class ApplicationController < ActionController::Base
            session[:auth] = { email: auth[:email], first_name: auth[:first_name], last_name: auth[:last_name] }
            redirect_to social_create_path
          else
-           gflash :error => "You must first sign in or sign up."
+           gflash error: 'You must first sign in or sign up.'
            redirect_to signin_path, flash: { error: 'You must first sign in or sign up.' }
          end
        end
@@ -98,34 +97,21 @@ class ApplicationController < ActionController::Base
     logger.debug "> STICK #{params}"
     params[:email] = session[:email]
     params[:api_key] = session[:api_key]
-    params[:host]    = Ind.http_api
+    stick_host
     logger.debug "> STICKD #{params}"
     params
   end
 
   def stick_host(_tmp = {}, _permitted_tmp = {})
-    logger.debug "> STICK #{params}"
-    params[:host]    = Ind.http_api
-    logger.debug "> STICKD #{params}"
+    params[:host] = Ind.http_api
     params
   end
 
   def stick_storage_keys(_tmp = {}, _permitted_tmp = {})
-    logger.debug "> STICK #{params}"
+    stick_keys
     params[:accesskey] = session[:storage_access_key]
     params[:secretkey] = session[:storage_secret_key]
     logger.debug "> STICKD #{params}"
-    params
-  end
-
-  def visit_access_keys(_tmp = {}, _permitted_tmp = {})
-    logger.debug "> STORAGE_ACCESS #{params}"
-    params[:email] = session[:email]
-    params[:api_key] = session[:api_key]
-    params[:host]    = Ind.http_api
-    params[:accesskey] = session[:storage_access_key]
-    params[:secretkey] = session[:storage_secret_key]
-    logger.debug "> STORAGE_ACCESS_KEYS #{params}"
     params
   end
 
@@ -137,11 +123,11 @@ class ApplicationController < ActionController::Base
     # notify  hipchat, send an email to support@megam.io which creates a support ticket.
     # redirect to the users last visited page.
     if !signed_in?
-    gflash :error => "#{mai}"
-    redirect_to(signin_path , flash: { api_error: 'api_error' }) && return
+      gflash error: "#{mai}"
+      redirect_to(signin_path, flash: { api_error: 'api_error' }) && return
     else
-      gflash :error => "#{mai}"
-      redirect_to(cockpits_path, flash: {api_error: 'api_error'} ) && return
+      gflash error: "#{mai}"
+      redirect_to(cockpits_path, flash: { api_error: 'api_error' }) && return
     end
   rescue ApplicationMailer::MegamSnailError => mse
     ascii_snail
@@ -177,5 +163,4 @@ class ApplicationController < ActionController::Base
     end
     logger.debug "\033[1m\033[35m..(*_*)...\033[0m\033[22m"
   end
-
 end
