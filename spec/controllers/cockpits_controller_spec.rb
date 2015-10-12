80ec5d9384989aec144679ca132cb253bf09abf9
vijaykanthm28 <mvijykanth@megam.io>
2015-10-12 10:24:06 +0530

    Rspec testing on going with CockpitsController

---
+++ spec/controllers/cockpits_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+include CatalogHelper
+
+RSpec.describe CockpitsController, :controller => true do
+
+  describe "GET index" do
+      #get :index
+     #expect(assigns(:assemblies_grouped)).to be_success
+    end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+# require 'rspec/autorun'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -29,4 +29,7 @@ config.infer_base_class_for_anonymous_controllers = false
 # the seed, which is printed after each run.
 # --seed 1234
 config.order = "random"
+
+config.backtrace_exclusion_patterns << /vendor\//
+config.backtrace_exclusion_patterns << /lib\/rspec\/rails/
 end
