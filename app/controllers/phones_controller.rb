21946fc103eb415643fe84013086c5ea675b690f
Thomas Alrin <thomasalrin@megam.co.in>
2016-01-07 20:26:29 +0530

    otp error case ok

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -3,7 +3,8 @@ class PhonesController < ApplicationController
   def new
    	pin_send = Nilavu::OTP::Infobip.new.send_pin("#{params['mobile_number']}")
 	if pin_send == "Message sending failed"
-		toast_error(signup_path, "OTP auth pin Message sending failed")
+		flash[:error] = 'Sorry! OTP-PIN sending Failed!'
+		render js: "window.location = '#{signup_path}'"
 	else
 		session[:otp_pin_id] = pin_send
 	end
@@ -12,7 +13,8 @@ class PhonesController < ApplicationController
   def create
 	pin_verify = Nilavu::OTP::Infobip.new.verify_pin(session[:otp_pin_id], params['pin'])
 	unless pin_verify
-		toast_error(signup_path, "OTP auth pin Message sending failed")
+		flash[:error] = 'Sorry! Your pin is wrong!'
+		render js: "window.location = '#{signup_path}'"
 	end
   end
 
