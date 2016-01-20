07f9bd25374d6ba7f49efe836f3355c466d1a221
indykish <nkishore@megam.io>
2016-01-20 16:45:30 +0530

    Nudgespot, its like intercom.io (billion dollar league)

--- app/assets/javascripts/megam.support.js
+++ app/assets/javascripts/megam.support.js
@@ -1,16 +1,6 @@
-// Start of Tawk.to Script
-var $_Tawk_API = {},
-  $_Tawk_LoadStart = new Date();
-(function() {
-  var s1 = document.createElement("script"),
-    s0 = document.getElementsByTagName("script")[0];
-  s1.async = true;
-  s1.src = 'https://embed.tawk.to/552254d2a329e2c841f9fb70/default';
-  s1.charset = 'UTF-8';
-  s1.setAttribute('crossorigin', '*');
-  s0.parentNode.insertBefore(s1, s0);
-})();
-//End of Tawk.to Script
+// Start of Nudegespot script
+(function(d,n){var s,a,p;s=document.createElement("script");s.type="text/javascript";s.async=true;s.src=(document.location.protocol==="https:"?"https:":"http:")+"//cdn.nudgespot.com"+"/nudgespot.js";a=document.getElementsByTagName("script");p=a[a.length-1];p.parentNode.insertBefore(s,p.nextSibling);window.nudgespot=n;n.init=function(t){function f(n,m){var a=m.split('.');2==a.length&&(n=n[a[0]],m=a[1]);n[m]=function(){n.push([m].concat(Array.prototype.slice.call(arguments,0)))}}n._version=0.1;n._globals=[t];n.people=n.people||[];n.params=n.params||[];m="track register unregister identify set_config people.delete people.create people.update people.create_property people.tag people.remove_Tag".split(" ");for(var i=0;i<m.length;i++)f(n,m[i])}})(document,window.nudgespot||[]);nudgespot.init("425d0d6f052a5ce5ae21931efa800675");
+//End of Nudgespot script
 
 //These are helper functions used by the UI
 function removeAt(selector_to_remove) {
@@ -25,6 +15,3 @@ function repWith(location, content_to_replace) {
   $(location).replaceWith(content_to_replace);
 }
 //end helper functions.
-$(document)
-    .ready(function () {
-    });
