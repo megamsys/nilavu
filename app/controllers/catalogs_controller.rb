470928a8a26cc4a17a2bfba7c61a1509f699196f
indykish <nkishore@megam.io>
2016-03-17 17:35:14 +0530

    Polished Collaboration overview, popup display of destroy

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -43,34 +43,12 @@ class CatalogsController < ApplicationController
     title: "#{params['req_action'].camelize}ing #{params['cattype'].downcase}"}
   end
 
-  #a confirmation question for a delete operation.
-  def kelvi
-    logger.debug "> Pilotable: kelvi"
-    @msg = { title: "Destroy #{params['cattype'].downcase}", message: "Do you want to destroy #{params['name']} ", redirect: catalog_path(:id => 1, :params => params)}
+    def kelvi
+      @msg = { title: "Destroy #{params['cattype'].downcase}", message: "Do you want to destroy #{params['name']} ", redirect: catalog_path(:id => 1, :params => params)}
   end
 
-  #this action performs a delete operation.
   def destroy
-    logger.debug "> Pilotable: destroy"
     Api::Requests.new.reqs(params)
-    @msg = { message: "Your  #{params['cattype'].downcase} #{params['name']} is getting nuked",
-    title: "#{params['req_action']}ing #{params['cattype'].downcase}"}
-  end
-
-  def runtime
-    logger.debug "> Pilotable: Runtime"
-    asm = Assembly.new.show(params).by_cattypes
-    @cattype = params["cattype"]
-    @appname = parse_key_value_json(asm['#{params["cattype"]}'].components[0][0].outputs, "id") if params["cattype"] == Api::Assemblies::MICROSERVICES
-    @host = parse_key_value_json(asm['#{params["cattype"]}'].components[0][0].outputs, "host") if params["cattype"] == Api::Assemblies::MICROSERVICES
-    @host = "" unless params["cattype"] == Api::Assemblies::MICROSERVICES
-    @appname = asm['"#{params["cattype"]}'].name + "." + parse_key_value_json(asm['#{params["cattype"]}'].inputs, "domain") unless params["cattype"] == Api::Assemblies::MICROSERVICES
-  end
-
-  def logs
-    logger.debug "> Pilotable: Logs"
-    asm = Assembly.new.show(params).by_cattypes
-    @appname = asm["#{params['cattype']}"].name + "." + parse_key_value_json(asm["#{params['cattype']}"].inputs, "domain")
-    @appname
+    @msg = { message: "Your  #{params['cattype'].downcase} #{params['name']} is getting nuked"}
   end
 end
