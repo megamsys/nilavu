adcdebd7016247574f5c20346537d5546e57a5fd
ranjithamca <ranjithar@megam.io>
2016-06-06 20:58:35 +0530

    fix metrics

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -12,14 +12,23 @@ class MetricsController < ApplicationController
 
 private
 def container(ip)
+  if ip == ""
+    return ""
+  else
   response = RestClient.get("http://#{ip}:9999/api/v1.3/containers")
   return  response.body
 end
 
+end
+
 
 def machine(ip)
+  if ip == ""
+    return ""
+  else
   response = RestClient.get("http://#{ip}:9999/api/v1.3/machine")
   return response.body
+end
 end
 
 end
