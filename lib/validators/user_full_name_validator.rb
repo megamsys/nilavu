3b14f18d85df7b0923b6c691efa86834bfd50ea4
indykish <nkishore@megam.io>
2016-08-05 20:03:28 +0530

    More tests getting added

--- lib/validators/user_full_name_validator.rb
+++
@@ -1,8 +0,0 @@
-class UserFullNameValidator < ActiveModel::EachValidator
-
-  def validate_each(record, attribute, value)
-    if SiteSetting.full_name_required && !record.name.present?
-      record.errors.add(attribute, :blank)
-    end
-  end
-end
---
+++ spec/components/json_error_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require_dependency 'json_error'
+
+shared_examples "a generic error" do
+  let(:result) { creator.create_errors_json(obj) }
+
+  it "should have a result object" do
+    expect(result).to be_present
+  end
+
+  it "has a generic error message" do
+    expect(result[:errors]).to eq([I18n.t('js.generic_error')])
+  end
+end
+
+describe JsonError do
+
+  let(:creator) { Object.new.extend(JsonError) }
+
+  describe "with a nil argument" do
+    it_behaves_like "a generic error" do
+      let(:obj) { nil }
+    end
+  end
+
+  describe "with an empty array" do
+    it_behaves_like "a generic error" do
+      let(:obj) { [] }
+    end
+  end
+
+  describe "with an activerecord object with no errors" do
+    it_behaves_like "a generic error" do
+      let(:obj) { Fabricate.build(:user) }
+    end
+  end
+
+  describe "with a string" do
+    it "returns the string in the error format" do
+      expect(creator.create_errors_json("test error")).to eq({errors: ["test error"]})
+    end
+  end 
+
+end
